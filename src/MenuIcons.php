c7ec4ca778212046e4a9071f70368b5333526358
Puleeno Nguyen <puleeno@gmail.com>
2021-07-01 05:14:57 +0700

    Create MenuIcons structure for Jankx

---
+++ src/MenuIcons.php
@@ -0,0 +1,16 @@
+<?php
+namespace Jankx\MenuIcons;
+
+class MenuIcons {
+    protected static $instance;
+
+    public static function get_instance() {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
+    private function __contruct() {
+    }
+}
